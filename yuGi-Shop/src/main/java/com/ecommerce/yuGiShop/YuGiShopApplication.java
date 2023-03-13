e171ae633397006c219fe855d18809fe80ae3a9a
Mohammed Sayeed Islam <120868662+mdsayeed133@users.noreply.github.com>
2023-03-13 09:02:44 -0700

    start up

---
+++ yuGi-Shop/src/main/java/com/ecommerce/yuGiShop/YuGiShopApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.yuGiShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuGiShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuGiShopApplication.class, args);
+	}
+
+}
---
+++ yuGi-Shop/src/test/java/com/ecommerce/yuGiShop/YuGiShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.yuGiShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuGiShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
