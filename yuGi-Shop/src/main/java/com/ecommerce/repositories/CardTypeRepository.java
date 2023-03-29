e052aaa6478d9131eed6ec83d49db9a7dc0313e4
Mohammed Sayeed Islam <120868662+mdsayeed133@users.noreply.github.com>
2023-03-29 16:28:41 -0700

    polish!!!

--- yuGi-Shop/src/main/java/com/ecommerce/repositories/CardTypeRepository.java
+++ yuGi-Shop/src/main/java/com/ecommerce/repositories/CardTypeRepository.java
@@ -2,6 +2,8 @@ package com.ecommerce.repositories;
 
 import com.ecommerce.model.CardType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CardTypeRepository extends JpaRepository<CardType, Integer> {
 }
--- yuGi-Shop/src/main/java/com/ecommerce/repositories/UsersRepository.java
+++ yuGi-Shop/src/main/java/com/ecommerce/repositories/UsersRepository.java
@@ -2,14 +2,12 @@ package com.ecommerce.repositories;
 
 import com.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface UsersRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsernameIgnoreCaseAndPassword(String username, String password);
-
-
-
 
 }
